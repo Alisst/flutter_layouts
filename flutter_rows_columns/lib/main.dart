f13d53b10a90a823305c72134761a2e70813ac9f
Ali Huzmeli <alihuzmeli_2005@hotmail.com>
2021-12-26 23:55:00 +0300

    Rows-Columns completed

---
+++ flutter_rows_columns/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rows and Columns',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Rows and Columns'),
+        ),
+        backgroundColor: Colors.indigo[100],
+        body: Home(),
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final sizeX = MediaQuery.of(context).size.width;
+    final sizeY = MediaQuery.of(context).size.height;
+
+    return Container(
+      width: sizeX,
+      height: sizeY,
+      child: Row(
+        children: createSquares(5),
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        // verticalDirection: VerticalDirection.up,
+        // textDirection: TextDirection.rtl,
+      ),
+    );
+  }
+
+  List<Widget> createSquares(int numSquares) {
+    int i = 0;
+    List colors = [
+      Colors.amber,
+      Colors.deepPurple,
+      Colors.deepOrange,
+      Colors.indigo,
+      Colors.lightBlue
+    ];
+
+    List<Widget> squares = [];
+    while (i < numSquares) {
+      Expanded square = Expanded(
+        flex: i,
+        child: Container(
+          color: colors[i],
+          width: 60,
+          height: 60,
+          child: Text(i.toString()),
+        ),
+      );
+      i++;
+      squares.add(square);
+    }
+    return squares;
+  }
+}
